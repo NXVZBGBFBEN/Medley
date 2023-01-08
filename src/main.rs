3c162ee829cb39148fe2d97614a54eb637fbd9e1
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-01-08 19:20:32 +0900

    標準出入力

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("入力してね");
+    let mut x = String::new();
+    io::stdin().read_line(&mut x).expect("失敗したから爆発するね");
+    print!("あなたの入力: {}", x);
 }
