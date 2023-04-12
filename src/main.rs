55835a1a47baa2fa6653a195fe4c765f4cc586f2
NXVZBGB FBEN <NXVZBGBFBEN@gmail.com>
2023-04-12 22:25:02 +0900

    エンジン選択部のベース機能実装

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,34 @@ use medley::lexer;
 use std::borrow::Borrow;
 use std::io;
 use std::io::Write;
+use dialoguer::Select;
+
+#[derive(Default)]
+struct Config {
+    engine: u8,
+}
+
+impl Config {
+    fn engine_select(&self) -> Result<Self, String> {
+        let engine_list = vec!["Internal", "Maxima"];
+        match Select::new().items(&engine_list).interact() {
+            Ok(x) => {
+                Ok(Self {
+                    engine: match x {
+                        0_usize => 0,
+                        1_usize => 1,
+                        _ => 2,
+                    }
+                })
+            }
+            Err(e) => Err(e.to_string()),
+        }
+    }
+}
 
 fn main() {
     println!("This is Medley, Version 1.2.0-alpha (2023-04-03)");
+    let mut config = Config::default();
     loop {
         print!("> ");
         io::stdout().flush().unwrap();
@@ -16,13 +41,25 @@ fn main() {
                     println!("bye");
                     break;
                 }
+                if input.trim() == "engine-select" {
+                    match config.engine_select() {
+                        Ok(x) => config = x,
+                        Err(e) => println!("{e}"),
+                    }
+                    continue;
+                }
                 match lexer::Lexer::lex(input.chars().collect()) {
                     Ok(tokens) => {
-                        if let Some(expr) = internal_engine::Parser::parse(tokens) {
-                            match internal_engine::eval(expr.borrow()) {
-                                Ok(result) => println!(" = {result}"),
-                                Err(calc_err) => println!(" = [CALC_ERR] {calc_err}"),
+                        match config.engine {
+                            0 => {
+                                if let Some(expr) = internal_engine::Parser::parse(tokens) {
+                                    match internal_engine::eval(expr.borrow()) {
+                                        Ok(result) => println!(" = {result}"),
+                                        Err(calc_err) => println!(" = [CALC_ERR] {calc_err}"),
+                                    }
+                                }
                             }
+                            _ => println!("?")
                         }
                     }
                     Err(syntax_err) => println!(" = [SYNTAX_ERR] {syntax_err}"),
