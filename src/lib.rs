1d2a3798f54bdf5d8e65f03601cebd7beeb3a237
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-10-11 17:51:46 +0900

    モジュール定義の追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 //! # Medley-API (`cfe_medley`)
 //! The CAS Front-End with LaTeX Syntax
 
+pub mod processor {
+    pub mod latex {
+        pub mod lexer;
+        pub mod parser;
+    }
+}
 pub mod internal_engine;
 pub mod lexer;
 pub mod config_manager;
